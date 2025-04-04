081a18f09aa03000cc39c9e8e9f51d8b98437ed5
Varun Narravula <varun@snare.dev>
2025-04-04 14:25:34 -0700

    feat: add devshell

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "stewdio - version audio stuff, with ease";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "stewdio-shell";
+          buildInputs = with pkgs; [
+            go
+            nodejs_22
+          ];
+        };
+      };
+    };
+}
